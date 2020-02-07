a2d28bd448b2fb7a6898e24e246b20fcecafa7a3
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2020-02-07 11:39:33 -0300

    Fix alert and rename some files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import AppNavigator from './AppNavigator';
+import AppNavigator from './appNavigator';
 
 import store from './store';
 
